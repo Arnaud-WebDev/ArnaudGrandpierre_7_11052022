81636c409001416002eee1f5f115640d521d4658
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-05-11 17:48:23 +0200

    Configuration du server sur le fichier server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express")
 //Importe le package cors
 const cors = require("cors")
 
+//Remplace body-parser()
 const app = express()
 
 let corsOption = {
@@ -15,4 +16,16 @@ app.use(cors(corsOption))
 //Permet d'utiliser le req.body utile pour les requêtes POST et PUT, envoie sous forme d'objet.
 app.use(express.json())
 
+//Il analyse les requêtes entrantes avec des charges utiles JSON et basé sur body-parser
 app.use(express.urlencoded({ extended: true }))
+
+//Défini un route simple a tester
+app.get("/", (req, res) => {
+  res.json({ message: "Je suis la route a tester" })
+})
+
+//Défini le port a ecouter
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Le serveur tourne sur le port ${PORT}`)
+})
