da5e498915a8c64024746301b967ac373d9a74ea
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-05-12 18:55:52 +0200

    finalisation de db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,4 +3,16 @@ module.exports = {
   USER: USER_MYSQL,
   PASSWORD: process.env.PASSWORD_MYSQL,
   DB: DB_MYSQL,
+  dialect: "mysql",
+  // pool est optionnelle mais je voulais tester
+  pool: {
+    // Nombre maximum de connexion dans le pool
+    max: 100,
+    // Nombre minimum de connexion dans le pool
+    min: 0,
+    // Durée maximale, en millisecondes, pendant laquelle ce pool essaiera d'obtenir une connexion avant de générer une erreur
+    acquire: 30000,
+    // Durée maximale, en millisecondes, pendant laquelle une connexion peut être inactive avant d'être libérée
+    idle: 10000,
+  },
 }
