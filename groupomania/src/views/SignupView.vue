b36e50eb2765a69d5481c1c6cdeeed14dd463910
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-05-15 11:23:06 +0200

    Implémentation de vue CLI et création de SignupView.vue et SgnUp.vue

---
+++ groupomania/src/views/SignupView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <SignUp />
+  </div>
+</template>
+
+<script>
+import SignUp from "@/components/SignUp.vue"
+
+export default {
+  name: "SignupView",
+  components: {
+    SignUp,
+  },
+}
+</script>
+
+<style></style>
