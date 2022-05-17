9b26943e19dc5766f36752bf734ca4c97cd8bef1
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2022-05-17 19:34:33 +0200

    Changer dans index.js de la page de démarrage

--- groupomania/src/router/index.js
+++ groupomania/src/router/index.js
@@ -14,13 +14,13 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const publicPages = ["/login", "/register", "/home"]
+  const publicPages = ["/register", "/login", "/home"]
   const authRequired = !publicPages.includes(to.path)
   const loggedIn = localStorage.getItem("user")
   // trying to access a restricted page + not logged in
   // redirect to login page
   if (authRequired && !loggedIn) {
-    next("/login")
+    next("/register")
   } else {
     next()
   }
